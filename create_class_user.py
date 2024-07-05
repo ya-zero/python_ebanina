c87e3e82c7f40d1527f701cd640f52af9646829f
ya-zero <yazero@yandex.ru>
2024-07-05 19:00:20 +0300

    Update create_class_user.py

--- create_class_user.py
+++ create_class_user.py
@@ -1,10 +1,22 @@
 class User():
-    def __init__(self,username,location,phone):
-        self.username=username
-        self.location=location
-        self.phone=phone
-    def __str__(self):
-        return self.username
-	
-vasy=User('vasy','russia','+7910')
-print(vasy.__dict__,type(vasy),vasy)
+
+    def __init__(self, username: str, location: str, phone: str):
+        self.username = username
+        self.location = location
+        self.phone = phone
+
+    def __str__(self) -> str:
+        return f'{type(self).__name__}.{self.username}'
+
+vasy = User('vasy', 'russia', '+7910')
+print('magic metod: %s \ntype: %s\nstr method class: %s' %
+      (vasy.__dict__, type(vasy), vasy))
+
+
+# from dataclasses import dataclass
+# @dataclass
+# class Book:
+#     title: str
+#     author: str
+# mybook = Book('1984', 'George Orwell')
+# print(mybook.__dict__,mybook)
